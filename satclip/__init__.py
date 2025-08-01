c4bcf8b1353ad3bfb3e87f457d84dbdc8b8a8b87
Ruth Crasto <ruthcrasto@microsoft.com>
2025-08-01 15:18:48 -0700

    Comment out unnecessary imports for load_lightweight

--- satclip/__init__.py
+++ satclip/__init__.py
@@ -1,5 +1,5 @@
-from . import *
-from .main import *
-from .model import *
-from .loss import *
+# from . import *
+# from .main import *
+# from .model import *
+# from .loss import *
 from .location_encoder import *
